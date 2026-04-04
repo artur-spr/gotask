fe9a8b4c15a3db21c6ab0a2495b97e18d8f40592
artur_spr <speranskyartur@gmail.com>
2026-04-04 22:28:15 +0500

    Now using go-homedir package.

---
+++ config/go.mod
@@ -0,0 +1,5 @@
+module github.com/artur-spr/gotask/config
+
+go 1.26.1
+
+require github.com/mitchellh/go-homedir v1.1.0
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"log"
+	// "os"
+	"path/filepath"
+
 	"github.com/artur-spr/gotask/cmd"
 )
 
